96c2ee80c4335a386beb03e02f0c79164397c8cf
ze-fernando <jfenginner935@gmail.com>
2023-07-29 18:36:06 -0300

    Upgrade urls

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.signin, name="signin"),
     path('signin/', views.signin, name="signin"),
     path('signup/', views.signup, name="signup"),
     path('home/', views.home, name="home"),
